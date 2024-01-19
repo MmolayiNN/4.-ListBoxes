971452c6e6d7ae50751185807782eadd5d336806
Hohm <Hohm@HOHM>
2024-01-19 13:36:52 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 23
+        ListBox1.Items.AddRange(New Object() {"iPhone", "Huawei", "Samsung", "Sony"})
+        ListBox1.Location = New Point(14, 31)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(168, 395)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(323, 55)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(189, 33)
+        Button1.TabIndex = 1
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(323, 125)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(189, 38)
+        Button2.TabIndex = 2
+        Button2.Text = "Remove"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(323, 197)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(189, 33)
+        Button3.TabIndex = 3
+        Button3.Text = "Delete Selected"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(323, 266)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(189, 33)
+        Button4.TabIndex = 4
+        Button4.Text = "Clear"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Red
+        ClientSize = New Size(900, 518)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Font = New Font("Segoe UI", 10.2F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+
+End Class
